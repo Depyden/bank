7ab7bad88b8434981d01c136d520b2a7b307e6d3
Denis Posmetnijs <Posmetnijs@gmail.com>
2021-04-03 12:52:39 +0300

    api half works

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,6 @@ import './styles/component.css';
 
 var requestOptions = {
 	method: 'GET',
-	redirect: 'follow',
 	mode:'cors',
 	headers: ("Access-Control-Allow-Methods: GET, POST, HEAD, OPTIONS, PUT, DELETE, PATCH")
   };
@@ -18,34 +17,30 @@ class Main extends React.Component{
 		};
 	  }
 
-	  componentDidMount() {
+	componentDidMount() {
 		fetch("https://hr.peterpartner.net/test/android/v1/users.json", requestOptions)
-		  .then(res => res.text())
-		  .then(result => console.log(result))
-		  .catch(error => console.log('error', error));
-/* 		  .then(
-			(result) => {
-			  this.setState({
-				isLoaded: true,
-				items: result.items
-			  });
-			},
-			// Примечание: важно обрабатывать ошибки именно здесь, а не в блоке catch(),
-			// чтобы не перехватывать исключения из ошибок в самих компонентах.
-			(error) => {
-			  this.setState({
-				isLoaded: true,
-				error
-			  });
-			}
-		  ) */
-
-	  }
-
-  
-
-
-
+			.then(
+				(response) => {
+					response.text();
+					console.log(response);
+				},
+				(result) => {
+					console.log('Готово');
+					this.setState({
+						isLoaded: true,
+						items: result.items
+					});
+				},
+				(error) => {
+					console.log('Ошибка', error);
+					this.setState({
+						isLoaded: true,
+						error
+					});
+				},
+			)
+	}
+	
 	render() {
 		const { error, isLoaded, items } = this.state;
 		if (error) {
@@ -53,13 +48,15 @@ class Main extends React.Component{
 		} else if (!isLoaded) {
 		  return <div>Загрузка...</div>;
 		} else {
+
+			//document.getElementById("CardNumber").innerHTML = items;
 			return (
 				<div className="Component">
 					<div className="StatusBar"></div>
 					<div className="Box"><header>Главная</header></div>
 					<div className="Card"></div>
 					<img id="Mastercard" src='./img/Groupmastercard.svg' alt="Mastercard"></img>
-					<h2>items.card_number</h2>
+					<p id = "CardNumber"></p>
 				</div>
 			);
 		}
